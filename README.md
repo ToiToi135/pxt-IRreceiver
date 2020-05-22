e9914c6319b723883afa5e278a7e79d24fb54382
ToiToi135 <65713407+ToiToi135@users.noreply.github.com>
2020-05-22 10:32:26 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
-# pxt-irreceiver
+ # KEY17_IR for micro-bit
 
+Extension for KEY17_IR
 
+Note: IR receiver for KEY17 receiver
 
-## TODO
+## License
 
-- [ ] Add a reference for your blocks here
-- [ ] Add "icon.png" image (300x200) in the root folder
-- [ ] Add "- beta" to the GitHub project description if you are still iterating it.
-- [ ] Turn on your automated build on https://travis-ci.org
-- [ ] Use "pxt bump" to create a tagged release on GitHub
-- [ ] On GitHub, create a new file named LICENSE. Select the MIT License template.
-- [ ] Get your package reviewed and approved https://makecode.microbit.org/extensions/approval
-
-Read more at https://makecode.microbit.org/extensions
+MIT
 
 ## Supported targets
 
 * for PXT/microbit
 (The metadata above is needed for package search.)
-
---
+++ enums.d.ts
@@ -0,0 +1,53 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum Pins {
+    P0 = 3,
+    P1 = 2,
+    P2 = 1,
+    P3 = 4,
+    P4 = 5,
+    P5 = 17,
+    P6 = 12,
+    P7 = 11,
+    P8 = 18,
+    P9 = 10,
+    P10 = 6,
+    P11 = 26,
+    P12 = 20,
+    P13 = 23,
+    P14 = 22,
+    P15 = 21,
+    P16 = 16,
+    P19 = 0,
+    P20 = 30,
+    }
+
+
+    declare enum RemoteButton {   
+ 	  NUM1 = 0x45,
+	  NUM2 = 0x46,
+	  NUM3 = 0x47,
+	  NUM4 = 0x44,
+	  NUM5 = 0x43,
+	  NUM6 = 0xd,
+	  NUM7 = 0x40,
+	  NUM8 = 0x19,
+	  NUM9 = 0x7,
+	  KOME = 0x15,
+	  NUM0 = 0x9,
+	  SHARP = 0x16,
+	  //NOT0 = 0xc,
+	  FORWARD = 0x18,
+	  //NOT1 = 0x5e,
+	  LEFT = 0x8,
+	  OK = 0x1c,
+	  RIGHT = 0x5a,
+	  //NOT2 = 0x42,
+	  BACK = 0x52,
+	  //NOT3 = 0x4A
+    }
+declare namespace KEY17_IR {
+}
+
+// Auto-generated. Do not edit. Really.
